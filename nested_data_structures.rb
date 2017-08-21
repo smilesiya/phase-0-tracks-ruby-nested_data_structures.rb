b654103bb471e7f51522d012dec96cac2d87e8a7
smilesiya <smilesiya11@gmail.com>
2017-08-21 03:42:00 -0500

    Add files via upload

---
+++ nested_data_structures.rb
@@ -0,0 +1,35 @@
+Disney_World_Orlando = {
+   Magic_Kingdom : {
+        main_theme: 'Mickey Mouse And Friends',
+        Gift_shops: {
+            total_outlets: 20,
+            most_popular_outlets: 5
+        },
+        Disney_charactors: [
+            "Mickey Mouse", 
+            "Goofy"
+        ]
+    },
+    Epcot: {
+        main_theme: 'Beach Club',
+        Gift_shops: {
+            total_outlets: 10,
+            most_popular_outlets: 4
+        },
+        Disney_charactors: [
+            "Nemo", 
+            "Doreey"
+        ]
+    },
+    Animal_Kingdom: {
+        main_theme: 'Anmimal and Wildlife',
+        Gift_shops: {
+            total_outlets: 10,
+            most_popular_outlets: 5
+        },
+        Disney_charactors: [
+            "Lion King", 
+            "Jungle Book"
+        ]
+    },
+}
